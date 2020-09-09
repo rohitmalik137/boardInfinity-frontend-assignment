a7e597e0d48be0b894bb3492e753ea7875ea9321
Rohit Malik <bpsrohitmalik@gmail.com>
2020-09-09 22:40:11 +0530

    converted classful components to functional components

--- src/components/inputForm/inputForm.component.jsx
+++ src/components/inputForm/inputForm.component.jsx
@@ -1,88 +1,76 @@
-import React, { useState } from 'react';
+import React from 'react';
 import moment from 'moment';
 import { connect } from 'react-redux';
 import uuid from 'react-uuid';
+import { useDispatch } from 'react-redux';
 
 import 'antd/dist/antd.css';
 import './inputForm.styles.scss';
 import { Form, Input, Button, DatePicker } from 'antd';
 
-class InputForm extends React.Component {
-  constructor(props) {
-    super(props);
+const InputForm = () => {
+  const dispatch = useDispatch();
 
-    this.state = {
-      tweet: '',
-      expiryDate: null,
-    };
-  }
-
-  onFinish = (values) => {
+  const onFinish = (values) => {
     const newTweet = {
       id: uuid(),
       tweet: values.tweet,
       expiryDate: values.expiryDate.toLocaleString(),
     };
-    this.props.dispatch({
+    dispatch({
       type: 'POST_TWEET',
       newTweet,
     });
   };
 
-  onFinishFailed = (errorInfo) => {
+  const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo);
   };
 
-  disabledDate = (current) => {
+  const disabledDate = (current) => {
     // Can not select days before today and today
     return current && current < moment().endOf('day');
   };
 
-  render() {
-    return (
-      <div className="form-container">
-        <Form
-          name="basic"
-          onFinish={this.onFinish}
-          onFinishFailed={this.onFinishFailed}
+  return (
+    <div className="form-container">
+      <Form name="basic" onFinish={onFinish} onFinishFailed={onFinishFailed}>
+        <Form.Item
+          label="Tweet"
+          name="tweet"
+          className="formData"
+          rules={[
+            {
+              required: true,
+              message: 'Tweet here!',
+            },
+          ]}
         >
-          <Form.Item
-            label="Tweet"
-            name="tweet"
-            className="formData"
-            rules={[
-              {
-                required: true,
-                message: 'Tweet here!',
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            label="DatePicker"
-            className="formData"
-            name="expiryDate"
-            rules={[
-              {
-                required: true,
-                message: 'Please input a valid ExpiryDate!',
-              },
-            ]}
-          >
-            <DatePicker
-              disabledDate={this.disabledDate}
-              format="YYYY-MM-DD HH:mm:ss"
-            />
-          </Form.Item>
-          <Button type="primary" htmlType="submit">
-            Tweet
-          </Button>
-        </Form>
-      </div>
-    );
-  }
-}
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="DatePicker"
+          className="formData"
+          name="expiryDate"
+          rules={[
+            {
+              required: true,
+              message: 'Please input a valid ExpiryDate!',
+            },
+          ]}
+        >
+          <DatePicker
+            disabledDate={disabledDate}
+            format="YYYY-MM-DD HH:mm:ss"
+          />
+        </Form.Item>
+        <Button type="primary" htmlType="submit">
+          Tweet
+        </Button>
+      </Form>
+    </div>
+  );
+};
 
 // const mapDispatchToProps = (dispatch) => ({
 //   postTweet: (tweet) => dispatch(postTweet(tweet)),
--- src/components/tweetsContainer/tweetsContainer.component.jsx
+++ src/components/tweetsContainer/tweetsContainer.component.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { useStore } from 'react-redux';
 
 import Tweet from '../single-tweet/single-tweet.component';
 import './tweetsContainer.styles.scss';
 
-class TweetsContainer extends React.Component {
-  render() {
-    return (
-      <div className="grid">
-        {this.props.tweet.tweet.tweets.map((tweet) => {
-          return (
-            <Tweet
-              key={tweet.id}
-              tweet={tweet.tweet}
-              expiryDate={tweet.expiryDate}
-            />
-          );
-        })}
-      </div>
-    );
-  }
-}
+const TweetsContainer = () => {
+  const store = useStore();
+  return (
+    <div className="grid">
+      {store.getState().tweet.tweets.map((tweet) => {
+        return (
+          <Tweet
+            key={tweet.id}
+            tweet={tweet.tweet}
+            expiryDate={tweet.expiryDate}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
